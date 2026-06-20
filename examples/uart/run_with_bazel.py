9100648d69c2333ed7de1bf7f5e8dfa4db65f6a2
Nocilis <292232246+N0cilis@users.noreply.github.com>
2026-06-20 21:56:29 +0200

    add licence header

--- examples/uart/run_with_bazel.py
+++ examples/uart/run_with_bazel.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
-"""
-VHDL UART
----------
-
-A more realistic test bench of an UART to show VUnit VHDL usage on a
-typical module.
-"""
+#  Copyright 2026 Nocilis
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 
 from sim.vunit_bazel_helper import get_vunit_from_bazel,add_lib_from_bazel
 from pathlib import Path
